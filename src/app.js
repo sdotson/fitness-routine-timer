b28ed174714c26dfed6c26258713d8ff413b40ce
sdotson <stu.dotson@gmail.com>
2017-04-29 17:36:46 -0400

    get started on layout

--- src/app.js
+++ src/app.js
@@ -2,20 +2,64 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   Text,
-  View
+  Picker,
+  View,
+  Button
 } from 'react-native';
 
 import { Header } from './components/common';
 
 export default class stretchingroutinetimer extends Component {
+  state = {
+
+  }
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Header headerText="Stretch Routine Timer" />
-        <Text>
-          Content
-        </Text>
+        <View style={styles.selectRoutine}>
+          <Text>
+            Select a Routine
+          </Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={"weee"}
+            onValueChange={(lang) => this.setState({language: lang})}>
+            <Picker.Item label="Java" value="java" />
+            <Picker.Item label="JavaScript" value="js" />
+            <Picker.Item label="Python" value="js" />
+          </Picker>
+        </View>
+        <View style={styles.createRoutine}>
+          <Button style={styles.button} title="Create a routine" onPress={()=>{}} />
+        </View>
       </View>
     );
+  }
+}
+
+const styles = {
+  container: {
+    paddingLeft: 5,
+    paddingRight: 5,
+    backgroundColor: '#ccc',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch'
+  },
+  selectRoutine: {
+    backgroundColor: 'orange',
+    alignItems: 'center',
+    justifyContent:'center',
+    flex: 1
+  },
+  picker: {
+    backgroundColor: 'purple',
+    alignSelf: 'stretch'
+  },
+  createRoutine: {
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    flex: 1
   }
 }
