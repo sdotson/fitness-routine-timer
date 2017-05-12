b32d6ee523379ca7e272852764e1532ae5928e91
sdotson <stu.dotson@gmail.com>
2017-05-12 12:40:34 -0400

    more progress, unfortunately need to leave to make lunch for everybody

--- src/components/CreateRoutine.js
+++ src/components/CreateRoutine.js
@@ -2,30 +2,38 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  Button
+  Button,
+  Picker,
+  Switch,
+  Modal
 } from 'react-native';
 
 import realm from '../db/realm';
 
-import { Header, Subheader } from './common';
+import { Header, Subheader, Input } from './common';
 
 export default class CreateRoutine extends Component {
 
   state = {
+    exercise: 'Rest',
     exercises: [],
-    routine: []
+    routine: [],
+    modalVisible: false
   };
 
   componentDidMount() {
     const exercises = realm.objects('Exercise');
-    console.log(exercises);
     this.setState({
       exercises: Array.from(exercises)
     });
   }
 
-  updateDataList(data) {
-    console.log('data', data);
+  onExerciseSelect() {
+    console.log('exercise selected');
+  }
+
+  onAddExerciseClick() {
+
   }
 
   render() {
@@ -33,18 +41,60 @@ export default class CreateRoutine extends Component {
       <View style={styles.container}>
         <Header headerText="Fitness Routine Timer" />
         <Subheader headerText="Create Routine" />
-        <Text>
-          Down below will be a form to create a new routine.
-        </Text>
-        <Text>
-          Will need a list of all exercises from database.
-          {this.state.exercises.map(ex => ex.name)}
-        </Text>
-        <Text>
-          will need field for routine name
-        </Text>
+        <Input
+          label="Routine Name"
+          placeholder="Enter routine name here"
+        />
+        <Button
+          style={styles.button}
+          onPress={() => { this.setState({modalVisible: true})}}
+          title={"Add to routine"}
+          color="#841584"
+        />
 
-      { this.state.routine.map(exercise => exercise.name) }
+
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+          <Text>Add exercise</Text>
+         <View style={{marginTop: 22}}>
+          <Picker
+            style={styles.picker}
+            selectedValue={this.state.exercise}
+            onValueChange={(exercise) => this.setState({exercise})}>
+              { this.state.exercises.map(exercise => <Picker.Item label={exercise.name} value={exercise.name} key={exercise.name} />) }
+          </Picker>
+          <Text>One side at a time</Text>
+          <Switch
+              onValueChange={(value) => {}}
+              value={true} />
+          <Input
+            label="Type"
+            placeholder="Enter rest, stretch, or exercise"
+          />
+          <Input
+            label="Duration"
+            placeholder="How long do you want to do this exercise?"
+          />
+          <Button
+            style={styles.button}
+            onPress={() => { this.setState({modalVisible: false})}}
+            title={"Add to routine"}
+            color="#841584"
+            />
+         </View>
+        </Modal>
+
+
+        <Text>
+          Current Routine
+        </Text>
+        <Text>
+          { this.state.routine.map(exercise => exercise.name + ', ') }
+        </Text>
       </View>
     );
   }
--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -25,18 +25,18 @@ const styles ={
     paddingLeft: 5,
     fontSize: 18,
     lineHeight: 23,
-    flex: 2
+    height: 23,
+    backgroundColor: '#ddd'
   },
   labelStyle: {
     paddingLeft: 20,
-    fontSize: 18,
-    flex: 1
+    fontSize: 18
   },
   containerStyle: {
-    height: 40,
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'pink',
+    paddingTop: 10,
+    paddingBottom: 10
   }
 }
 
