09f7cdf79d53191a52a0ab35059dfb8741413330
sdotson <stu.dotson@gmail.com>
2017-05-20 10:08:40 -0400

    clear newRoutine store after adding new routine

--- src/reducers/newroutine.js
+++ src/reducers/newroutine.js
@@ -1,11 +1,14 @@
 import {
-  ADD_EXERCISE_TO_ROUTINE
+  ADD_EXERCISE_TO_ROUTINE,
+  ADD_ROUTINE
 } from '../actions/types';
 
 export default function(state = [], action) {
   switch(action.type) {
     case ADD_EXERCISE_TO_ROUTINE:
       return [...state, action.payload];
+    case ADD_ROUTINE:
+      return [];
   }
 
   return state;
