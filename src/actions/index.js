6cf75350280291c057f2f0a7dd7e9e24a4360bad
sdotson <stu.dotson@gmail.com>
2017-05-22 13:10:43 -0400

    remove axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,37 +1,8 @@
-import axios from 'axios';
-import store from 'react-native-simple-store';
-
 import {
-  GET_EXERCISES,
   ADD_EXERCISE_TO_ROUTINE,
   DELETE_EXERCISE_FROM_ROUTINE,
-  GET_ROUTINES,
   ADD_ROUTINE
 } from './types';
-
-export function getExercises() {
-  const request = axios({
-    method: 'get',
-    url: ''
-  });
-
-  return {
-    type: GET_EXERCISES,
-    payload: request
-  };
-}
-
-export function getRoutines() {
-  const request = axios({
-    method: 'get',
-    url: ''
-  });
-
-  return {
-    type: GET_ROUTINES,
-    payload: request
-  };
-}
 
 export function addExerciseToRoutine(exercise) {
   return {
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,6 +1,3 @@
-export const GET_EXERCISES = 'get_exercises';
 export const ADD_EXERCISE_TO_ROUTINE = 'add_exercise_to_routine';
 export const DELETE_EXERCISE_FROM_ROUTINE = 'delete_exercise_from_routine';
-
-export const GET_ROUTINES = 'get_routines';
 export const ADD_ROUTINE = 'add_routine';
